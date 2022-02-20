48ff55d6ddb945f03d3bed73184218367752b071
kritical0613 <kritical0613@outlook.com>
2022-02-20 05:40:12 -0600

    feat: back light

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Background from './components/Background';
 import Header from './components/Header';
 import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
+      <Background />
       <Header />
       <Hero />
     </div>
